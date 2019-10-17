5583e354506d259f24b22c47b82bfa50c0485d1e
Smith <smithwebdd@gmail.com>
2019-10-17 16:50:11 -0400

    simplified Hand methods for 'get_value'

--- hand.rb
+++ hand.rb
@@ -25,14 +25,20 @@ class Hand
 
   def get_value
     #get rank of card, put in an array
-    card_ranks = []
-    dealt_cards.each { |card| card_ranks.push(card.rank) }
+    #card_ranks = []
+    #dealt_cards.each { |card| card_ranks.push(card.rank) }
     
-    value = 0
-    card_ranks.each do |rank|
-      rank = rank.to_sym  #Convert string to symbol
-      value = value + VALUES[rank]
-    end
+    #card_ranks = dealt_cards.map { |card| card.rank }
+    card_ranks = dealt_cards.collect { |card| card.rank }
+
+
+    # value = 0
+    # card_ranks.each do |rank|
+    #   rank = rank.to_sym  #Convert string to symbol
+    #   value = value + VALUES[rank]
+    # end
+    #reduce, acc = 0, next interation sets acc to result of first interation and does the calc 
+    value = card_ranks.reduce(0) { |acc, rank| acc + VALUES[rank.to_sym] }
     value
   end
 end
